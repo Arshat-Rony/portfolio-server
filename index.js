6662fa13e4e6af9e7426019a4a71e9be9279c620
Rony <frrony1996@gmail.com>
2022-06-01 00:42:56 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+const port = process.env.port || 5000;
+
+
+
+// middleware 
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pu0vp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect()
+
+        const database = client.db("arshat-rony");
+        const projectCollection = database.collection("projects");
+        const skillsCollection = database.collection("skills");
+
+
+
+        // get all the projects
+        app.get("/projects", async (req, res) => {
+            const result = await projectCollection.find({}).toArray()
+            res.send(result)
+        })
+
+
+
+
+        // get all the skills
+        app.get("/skills", async (req, res) => {
+            const result = await skillsCollection.find({}).toArray()
+            res.send(result)
+        })
+
+
+
+
+    }
+    finally { }
+}
+
+run().catch(console.dir)
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('my portfolio')
+})
+
+app.listen(port, () => {
+    console.log("portfoio listening")
+})
